fbc7fb05f7ad716d9ade893447a9ea7fb3aa81cd
Franco Valencia <franco.avalencia@gmail.com>
2017-05-05 14:28:36 -0500

    Updated README file

--- README.md
+++ README.md
@@ -2,17 +2,28 @@
 
 This project is focused on performing sentiment analysis for the cryptocurrency bitcoin, but can easily be expanded to other coins. Currently it only gathers data from Twitter.
 
-# Dependencies:
+## Justification:
 
+Cryptocurrencies have been on the rise for the past years. Its value is completely defined by speculation of how much they can be worth. As such their exchange rates are very volatile, and very unpredictable.
 
-## Things to try:
+Twitter is one of the most popular social media networks there is, and its an outlet though which millions of people broadcast their thoughts, fears and opinions on any topic of their interest.
+
+The goal of this project is to determine if we analyze this publicly available data to develop cryto trading strategies or algorithms that are profitable.
+
+## Documentation:
+
+### Dependencies:
+
+## Analysis:
+
+## Variables to consider:
 
 * Stop lowercasing tweets
 
 * Allow emoticons for since its support by vader
 
 
-## Biography
+# Biography
 
 * http://www.cs.cmu.edu/~nasmith/papers/oconnor+balasubramanyan+routledge+smith.icwsm10.pdf
 
--- grapher.py
+++ grapher.py
@@ -8,40 +8,88 @@ from pymongo import MongoClient
 from poloniex import Poloniex
 
 polo = Poloniex()
+
+# Mongo DB
 client = MongoClient()
 db = client["cryto_trading_bot"]
+
+def date_to_timestamp(date):
+    return int(time.mktime(date.timetuple())) - 18000
+
+def get_twitter_dataset(start, end):
+
+    tweets = db.bitcoin_sentiment.find(
+            {'created': {'$gte': start, '$lt': end}}
+            )
+
+    df = pd.DataFrame(
+            list(tweets)
+            )
+
+    df = df.set_index('created')
+
+    compound = df['s_compound']
+    compound = compound.resample("5T").mean().fillna(0)
+
+    return compound
+
+def get_poloniex_dataset(start, end):
+    start_timestamp = date_to_timestamp(start)
+    end_timestamp = date_to_timestamp(end)
+
+    chart_data = (polo.returnChartData (
+        'USDT_BTC', 300, start_timestamp, end_timestamp
+        ))
+
+    pol_data = pd.DataFrame(chart_data)
+
+    pol_data['date'] = map (
+            lambda x: datetime.fromtimestamp(int(x)) + timedelta(hours=5),
+            pol_data['date'])
+
+    pol_data = pol_data.set_index('date')
+    average = pol_data['weightedAverage'].astype(float)
+
+    return average
+
 
 def main(start_time, end_time):
 
     start = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
     end = datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
 
-    start_timestamp = int(time.mktime(start.timetuple())) - 18000
-    end_timestamp = int(time.mktime(end.timetuple())) - 18000
+    twitter_ds = get_twitter_dataset(start, end)
+    poloniex_ds = get_poloniex_dataset(start, end)
 
-    tweets = db.bitcoin_sentiment.find({'created': {'$gte': start, '$lt': end}})
+    print (twitter_ds)
 
-    data = pd.DataFrame(list(tweets))
-    chart_data = (polo.returnChartData ('USDT_BTC', 300, start_timestamp, end_timestamp))
+    if (not twitter_ds.empty):
+        # fig, ax1 = plt.subplots()
+        # t = twitter_ds.index.values
+        # s1 = twitter_ds.iloc[0]
+        # ax1.plot(t, s1, 'b-')
+        # ax1.set_xlabel('time (s)')
+    # Make the y-axis label, ticks and tick labels match the line color.
+        # ax1.set_ylabel('exp', color='b')
+        # ax1.tick_params('y', colors='b')
 
-    pol_data = pd.DataFrame(chart_data)
-    pol_data['date'] = map (lambda x: datetime.fromtimestamp(int(x)) + timedelta(hours=5), pol_data['date'])
-    pol_data['weightedAverage'] = pol_data['weightedAverage'].astype(float)
+        # ax2 = ax1.twinx()
+        # s2 = poloniex_ds.iloc[0]
+        # ax2.plot(t, s2, 'r.')
+        # ax2.set_ylabel('sin', color='r')
+        # ax2.tick_params('y', colors='r')
 
-    print (pol_data)
-    print (data['created'])
-
-    if (not data.empty):
-        data.plot(
-                x = 'created',
-                y = 's_compound',
-                kind='bar',
+        # fig.tight_layout()
+        # plt.show()
+        ax1 = twitter_ds.plot(
+                kind='line',
                 color='blue')
-        pol_data.plot(
-                x = 'date',
-                y = 'weightedAverage',
+        ax2 = ax1.twinx()
+        poloniex_ds.plot(
                 kind = 'line',
-                color ='red')
+                color ='red',
+                ax=ax2
+                )
         plt.show()
     else:
         print('Not enough data for that timeline')
